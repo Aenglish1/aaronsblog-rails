2001d9639e08aea420201f05c99decc9d5ca03d3
Aaron English <aaron.h.english@gmail.com>
2018-04-10 10:51:01 -0500

    Update posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,14 @@ class PostsController < ApplicationController
 before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
-  end
+    if params[:category].blank?
+  @posts = Post.all.order("created_at DESC")
+
+else
+@category_id = Category.find_by(name: params[:category]).id
+@posts = Post.where(category_id: @category_id).order("created_at DESC")
+    end
+end
 
   def show
   end
@@ -28,15 +34,15 @@ before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def update
      if @post.update(post_params)
-          redirect_to @post, notice: “Update successful”
+          redirect_to @post, notice: "Update successful"
      else
-          render ‘edit’
+          render 'edit'
      end
 end
 
 def destroy
      @post.destroy
-     redirect_to root_path, notice: “Post destroyed”
+     redirect_to root_path, notice: "Post destroyed"
 end
 
 private
